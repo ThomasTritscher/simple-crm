a0f5f2803476eeddb1a7857072128d7f374d21a8
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-05-22 12:07:27 +0200

    g c edit-user and edit-user-address

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/models/user.class';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,33 +16,35 @@ export class UserDetailComponent implements OnInit {
 
   userId = '';
   user: User = new User();
-  
 
-  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
+
+  constructor(private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( paramMap => {
+    this.route.paramMap.subscribe(paramMap => {
       this.userId = paramMap.get('id') || '';
       console.log('Got ID', this.userId);
       this.getUser();
     })
   }
 
-  getUser(){
+  getUser() {
     this.firestore
-    .collection('users')
-    .doc(this.userId)
-    .valueChanges()
-    .subscribe((user:any) => {
-      this.user = new User(user);//Json switch into Obj typ User
-      console.log('Retrieved User', this.user)
-    });
+      .collection('users')
+      .doc(this.userId)
+      .valueChanges()
+      .subscribe((user: any) => {
+        this.user = new User(user);//Json switch into Obj typ User
+        console.log('Retrieved User', this.user)
+      });
   }
-  editUserDetail(){
-    
+  editUserDetail() {
+    this.dialog.open(DialogEditUserComponent);
   }
-  editMenu(){
-
+  editMenu() {
+    this.dialog.open(DialogEditAddressComponent);
   }
 
 }
